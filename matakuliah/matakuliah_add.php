e158bc2fa5f68073f5fd5e22b9d94d3acb7a00d0
Maulana0012 <waytrial03@gmail.com>
2024-06-08 00:44:37 +0800

    new file add matakuliah

--- matakuliah/matakuliah_add.php
+++ matakuliah/matakuliah_add.php
@@ -1,56 +1,55 @@
 <?php
 if (isset($_POST['Submit'])) {
-    $nim = $_POST['nim'];
-    $nama = $_POST['nama'];
-    $alamat = $_POST['alamat'];
-    $jk = $_POST['jenis_kelamin'];
-    $email = $_POST['email'];
-    $telepon = $_POST['telepon'];
-    $result = mysqli_query($conn, "INSERT INTO mahasiswa(nim,nama,alamat,jenis_kelamin,email,telpon)
-VALUES('$nim','$nama','$alamat','$jk','$email','$telepon')");
-    header("Location:?page=mahasiswa-show");
+    $kode_matkul = $_POST['kode_matkul'];
+    $nama_matkul = $_POST['nama_matkul'];
+    $sks = $_POST['sks'];
+    $semester = $_POST['semester'];
+    $result = mysqli_query($conn, "INSERT INTO matakuliah(kode_matkul, nama_matkul, sks, semester)
+VALUES('$kode_matkul','$nama_matkul','$sks','$semester')");
+    header("Location:?page=matakuliah-show");
 }
 ?>
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <strong>Tambah Data Mahasiswa</strong>
+                <strong>Tambah Data matakuliah</strong>
             </div>
             <div class="card-body">
-                <form method="POST" action="?page=mahasiswa-add" class="form-horizontal">
+                <form method="POST" action="?page=matakuliah-add" class="form-horizontal">
                     <div class="form-group">
-                        <label for="nim" class="control-label">NIM</label>
-                        <input type="text" class="form-control" name="nim" placeholder="Masukan NIM..." required>
+                        <label for="kode_matkul" class="control-label">Kode Matakuliah</label>
+                        <input type="text" class="form-control" name="kode_matkul"
+                            placeholder="Masukan Kode Matakuliah..." required>
                     </div>
                     <div class="form-group">
-                        <label for="nama" class="ontrol-label">Nama Lengkap</label>
-                        <input type="text" class="form-control" name="nama" placeholder="Masukan Nama Lengkap..."
-                            required>
+                        <label for="nama_matkul" class="ontrol-label">Nama Matakuliah</label>
+                        <input type="text" class="form-control" name="nama_matkul"
+                            placeholder="Masukan Nama Matakuliah..." required>
                     </div>
                     <div class="form-group">
-                        <label for="nama" class="control-label">Jenis Kelamin</label>
-                        <select class="form-control" name="jenis_kelamin">
-                            <option disabled selected> Pilih </option>
-                            <option value="Pria">Pria</option>
-                            <option value="Wanita">Wanita</option>
+                        <label for="sks" class="control-label">SKS</label>
+                        <select class="form-control" name="sks">
+                            <option disabled selected> SKS </option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="2">3</option>
+                            <option value="2">4</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="alamat" class="control-label">Alamat</label>
-                        <input type="text" class="form-control" name="alamat" placeholder="Masukan Alamat..." required>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="email" class="control-label">Email</label>
-                            <input type="email" class="form-control" name="email" placeholder="Masukan email..."
-                                required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="telepon" class="control-label">Telepon</label>
-                            <input type="number" class="form-control" name="telepon" placeholder="Masukan telepon..."
-                                required>
-                        </div>
+                        <label for="semester" class="control-label">Semester</label>
+                        <select class="form-control" name="semester">
+                            <option disabled selected> Semester </option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="2">3</option>
+                            <option value="2">4</option>
+                            <option value="2">5</option>
+                            <option value="2">6</option>
+                            <option value="2">7</option>
+                            <option value="2">8</option>
+                        </select>
                     </div>
                     <input type="submit" name="Submit" class="btn btn-primary" value="Simpan">
                     <input type="reset" name="reset" class="btn btn-warning" value="Reset">
